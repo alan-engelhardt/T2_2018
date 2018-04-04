73bf005ffad451c9bf49c74a1bac45812ce18f8d
Alan Engelhardt <Alan@kkj-560.kea.local>
2018-04-04 11:49:13 +0200

    add toggle class

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,7 @@
-let job = "web dev";
-let where = "Copenhagen";
-let partner = "Julio";
-let children = 5;
-let future = "I will be a " + job + " in " + where + ". I will marry " + partner + " and we will have " + children + " children";
-
 let headLevel1 = document.querySelector("#myHeader");
 
 headLevel1.addEventListener("click", changeTextContent);
 
-console.log(headLevel1.textContent);
-
-//headLevel1.textContent = future;
-
 function changeTextContent(){
-	headLevel1.textContent = future;
+	headLevel1.classList.toggle("flash");
 }
