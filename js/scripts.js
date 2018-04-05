52176b65a142dba491f04018d01f48306a72eeaa
Alan Engelhardt <Alan@windows-149uopc.kea.local>
2018-04-05 14:23:50 +0200

    toggle colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
-let headLevel1 = document.querySelector("#myHeader");
+let header1 = document.querySelector("#header1");
+let header2 = document.querySelector("#header2");
+let hero = document.querySelector("#hero");
+let counter = 0;
 
-headLevel1.addEventListener("click", changeTextContent);
+header1.addEventListener("click", changeHeader);
+header2.addEventListener("click", changeHeader);
+hero.addEventListener("click", changeHero);
 
-function changeTextContent(){
-	headLevel1.classList.toggle("flash");
+window.addEventListener('scroll', e => {
+	header1.textContent = window.scrollX;
+	header2.textContent = window.scrollY;
+});
+
+function changeHeader() {
+	this.classList.toggle("flash");
+}
+
+function changeHero() {
+	counter++;
+	console.log(counter);
+	hero.classList = "";
+	if (counter == 1) {
+		hero.classList.add("red");
+	} else if (counter == 2) {
+		hero.classList.add("green");
+	} else if (counter == 3) {
+		hero.classList.add("blue");
+	} else if (counter == 4) {
+		hero.classList.add("orange");
+	} else if (counter == 5) {
+		hero.classList.add("purple");
+	}else{
+		counter=0;
+	}
 }
